5c0d119ab064f1f006b4ade543b8077cbeee9091
Evan Rysdam <evan@rysdam.org>
2019-11-17 05:34:12 -0500

    Showed that the screen is 640x480.

--- main.s
+++ main.s
@@ -10,12 +10,30 @@
 	.globl _start
 
 _start:
+	// Color the top of the screen cyan
+	
 	bl	GetFrameBufferPointerQEMU
-	ldr	r1, =0x000F000F
+	ldr	r1, =0x07FF		// r1 . color
+	mov	r2, #640		// r2 . number of pixels to write
+
 write_iter$:
-	str	r1, [r0]
-	add	r0, #4
-	b	write_iter$
+	strh	r1, [r0]
+	add	r0, #2
+
+	subs	r2, #1
+	bne	write_iter$
+
+	// Color the left side of the screen red
+
+	bl	GetFrameBufferPointerQEMU
+	ldr	r1, =0xF800
+	mov	r2, #480
+write_iter2$:
+	strh	r1, [r0]
+	add	r0, #1280
+
+	subs	r2, #1
+	bne	write_iter2$
 
 plain_loop$:
 	b	plain_loop$
