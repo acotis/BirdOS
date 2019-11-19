af5aaf6698c86a84a4ba49c288169005bf148a01
Evan Rysdam <evan@rysdam.org>
2019-11-19 04:37:49 -0500

    Added draw_textline method for drawing lines of text on predetermined guides.

--- main.s
+++ main.s
@@ -1,8 +1,7 @@
 
 // Imports
 
-	.globl draw_string
-	.globl draw_num_hex_32
+	.globl draw_textline
 	
 // Exports
 
@@ -41,73 +40,22 @@ _start:
 	ldr	sp, =0x18000		// STACK BASE
 	
 	ldr	r0, =String
-	mov	r1, #500
-	mov	r2, #460	
+	mov	r1, #0
+	mov	r2, #0	
 	ldr	r3, =0x000F0FFF	
-	bl	draw_string
+	bl	draw_textline
 
-	ldr	r0, =0x20000000
-wait_loop$:
-	subs	r0, #1
-	bne	wait_loop$
-	
-	
-	mov	r4, #-1
-	mov	r5, #10			// r5 . row offset
-	mov	r6, #10			// r6 . column offset
-memread_loop$:
-	add	r4, #1
-	ldrb	r0, [r4]
+	ldr	r0, =String
+	mov	r1, #2
+	mov	r2, #0
+	ldr	r3, =0xFFFF0000
+	bl	draw_textline
 
-	teq	r0, #0x1e // letter a (maybe)
-	beq	memread_do_print$
-
-	teq	r0, #0x02 // value 2 (continued key press)
-	beq	memread_do_print$
-
-	lsls	r1, r4, #6
-	beq	memread_do_print$
-
-	ldr	r1, =0x3F006000
-	cmp	r4, r1
-	bhi	plain_loop$
-	
-	sub	r1, #10
-	cmp	r4, r1
-	bhi	memread_do_print$
-	
-	b	memread_loop$
-	
-memread_do_print$:	
-	
-	add	r1, r6, #70		// Draw r0 (contents) in right column
-	mov	r2, r5
-	ldr	r3, =0x0000F800
-	bl	draw_num_hex_32
-
-	mov	r0, r4			// Draw r4 (address) in left column
-	mov	r1, r6
-	mov	r2, r5
-	ldr	r3, =0x0000FFFF
-	bl	draw_num_hex_32
-
-	add	r5, #16			// Move to next row
-	cmp	r5, #460		// Move to next column if needed
-	movhi	r5, #10
-	addhi	r6, #150
-
-	cmp	r6, #500
-	bhi	plain_loop$
-	
-	b	memread_loop$
-
+	ldr	r0, =String
+	mov	r1, #1
+	mov	r2, #0
+	ldr	r3, =0xF800FFFF
+	bl	draw_textline
 	
 plain_loop$:
 	b	plain_loop$
-
-fill_screen$:
-	bl	GetFrameBufferPointerQEMU
-	ldr	r1, =0xFFFF
-fill_loop$:
-	strh	r1, [r0], #2
-	b	fill_loop$
--- text/text.s
+++ text/text.s
@@ -12,7 +12,39 @@
 	.section .data
 	.align	2
 
-LineHeight:
-	.int 16		// Char height + gap between lines
+Offset:
+	.int	2	// x-offset of first row of text
+	.int	4	// y-offset of rirst row of text
 
+// Code
 
+// draw_textline: draw a string located at r0 on row r1 and column r2 using
+// colors r3.
+//
+//	r0 . address of null-terminated string to draw
+//	r1 . row to start on
+//	r2 . column to start on
+//	r3 . bg color (top 16 bits) and fg color (bottom 16 bits)
+	
+	
+draw_textline:
+	push	{r4, r5, lr}
+
+	ldr	r5, =Offset
+	ldr	r4, [r5]		// r4 . x-offset of first row
+	ldr	r5, [r5, #4]		// r5 . y-offset of first row
+	
+	lsl	r2, #3			// (character width is always 8)
+	add	r2, r4			// r2 . x-offset of text
+
+	ldr	r4, =CharacterHeight
+	ldr	r4, [r4]		// r4 . character height
+	mul	r1, r4			
+	add	r1, r5			// r1 . y-offset of text
+	
+	mov	r4, r1			// Swap r1 and r2 (xy vs rc)
+	mov	r1, r2
+	mov	r2, r4
+
+	bl	draw_string
+	pop	{r4, r5, pc}
