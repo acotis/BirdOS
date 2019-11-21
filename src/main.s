3a56663e2abe4930e783fdf20d24d12cfdcceb01
Evan Rysdam <evan@rysdam.org>
2019-11-20 21:37:59 -0500

    Added intelligent line-wrapping, including understanding newline characters.

--- src/main.s
+++ src/main.s
@@ -21,12 +21,16 @@ Line4:
 	.zero 65
 
 String:
-	.asciz "Hello world, this is a program (by Evan Rysdam). minimum. joke. kaj"
+	.asciz "Hello world, this is a program (by Evan Rysdam). minimum. joke. kaj. Also, now I've implemented smart line-wrapping, so I don't have to constantly split the strings I want to split into lots of pieces. It just handles it automatically. I can even put in newline characters myself,\nlike this."
+	
 String2:
 	.asciz "\"Shh,\" he said. \"It goes without saying!\"."
 
 String3:
 	.asciz "\"Ain't\" isn't a word."
+
+String4:
+	.asciz "0         1         2         3         4         5         6         7        0123456789012345678901234567890123456789012345678901234567890123456789012345678"
 	
 // Code
 
@@ -38,8 +42,8 @@ _start:
 
 	// Fill "String" with every extended-ascii char code
 	
-	mov	r0, #1			// r0 . offset
-	mov	r1, #1			// r1 . number to write
+	mov	r0, #32			// r0 . offset (skip unprintables)
+	mov	r1, #32			// r1 . number to write
 	ldr	r2, =Line1
 s_fill_loop$:	
 	strb	r1, [r2, r0]
@@ -57,9 +61,9 @@ s_fill_loop$:
 	// Draw the string as a line of text
 	
 	ldr	r0, =Line1
-	add	r0, #1
+	add	r0, #32
 	mov	r1, #0
-	mov	r2, #1	
+	mov	r2, #32	
 	ldr	r3, =0x000006FF	
 	bl	draw_textline
 
@@ -88,15 +92,21 @@ s_fill_loop$:
 	bl	draw_textline
 
 	ldr	r0, =String2
-	mov	r1, #7
+	mov	r1, #11
 	mov	r2, #0
 	ldr	r3, =0x0000FFFF
 	bl	draw_textline
 
 	ldr	r0, =String3
-	mov	r1, #9
+	mov	r1, #13
 	mov	r2, #0
 	ldr	r3, =0x0000FFFF
+	bl	draw_textline
+
+	ldr	r0, =String4
+	mov	r1, #15
+	mov	r2, #0
+	ldr	r3, =0x4208F800
 	bl	draw_textline
 	
 s_plain_loop$:
--- src/text/text.s
+++ src/text/text.s
@@ -13,21 +13,30 @@
 	.align	2
 
 Offset:
-	.int	2	// x-offset of first row of text
+	.int	4	// x-offset of first row of text
 	.int	4	// y-offset of rirst row of text
 
+LineWidth:
+	.int	79	// Number of characters per line
+
+LineBuffer:
+	.zero	81	// Buffer for up to a single screen-line of text
+	
+	
 // Code
 
-// draw_textline: draw a string located at r0 on row r1 and column r2 using
-// colors r3.
+	.section .text
+	.align 2
+	
+// draw_textline_nowrap: draw a string located at r0 on row r1 and column r2
+// using colors r3. Line wrapping not supported by this method.
 //
 //	r0 . address of null-terminated string to draw
 //	r1 . row to start on
 //	r2 . column to start on
 //	r3 . bg color (top 16 bits) and fg color (bottom 16 bits)
 	
-	
-draw_textline:
+draw_textline_nowrap:	
 	push	{r4, r5, lr}
 
 	ldr	r5, =Offset
@@ -48,3 +57,67 @@ draw_textline:
 
 	bl	draw_string
 	pop	{r4, r5, pc}
+
+// draw_textline: draw a string located at r0 on row r1 and column r2 with
+// colors r3. If necessary, wrap the line intelligently to the next row and
+// start back at column 0.	
+//
+//	r0 . location of the string to draw
+//	r1 . row to start on
+//	r2 . column to start on
+//	r3 . bg color (top 16 bits) and fg color (bottom 16 bits)
+	
+draw_textline:
+	push	{r4, r5, r6, r7, r8, r9, r10, lr}
+
+	mov	r4, r0			// r4 . location of string
+	mov	r5, r1			// r5 . row
+	mov	r6, r2			// r6 . column
+	mov	r7, r3			// r7 . color
+	
+	// Copy one line of text to the line buffer
+
+	mov	r10, #0			// r10 . found null char yet?
+	
+dt_buffer_loop$:
+	ldr	r8, =LineBuffer		// r8 . address of line buffer
+
+	ldr	r9, =LineWidth		// r9 . number of characters to copy
+	ldr	r9, [r9]
+	sub	r9, r6
+	
+dt_charcopy_loop$:
+	ldrb	r0, [r4], #1		// r0 = next char to copy
+	strb	r0, [r8]
+	
+	cmp	r0, #0			// If null char, set flag and flush
+	moveq	r10, #1
+	beq	dt_flush_buffer$
+
+	cmp	r0, #10			// If newline character, put a null
+	moveq	r0, #0			// character and flush
+	strb	r0, [r8]
+	beq	dt_flush_buffer$		
+	
+	subs	r9, #1			// If end of line, flush
+	beq	dt_flush_buffer$
+
+	add	r8, #1			// Else, march forward in buffer and
+	b	dt_charcopy_loop$	// loop back to charcopy 
+	
+dt_flush_buffer$:	
+	ldr	r0, =LineBuffer		// Draw the line buffer to the screen
+	mov	r1, r5
+	mov	r2, r6
+	mov	r3, r7
+	bl	draw_textline_nowrap
+
+	add	r5, #1			// Go to the next line
+	mov	r6, #0			// Go to the first column
+
+	cmp	r10, #0			// If r10 (found null char) not set,
+	beq	dt_buffer_loop$		// loop back again
+
+	// Done
+
+	pop	{r4, r5, r6, r7, r8, r9, r10, pc}
