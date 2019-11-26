45f4aae5a8e5be01158c23dd443d9e90c31448d7
Evan Rysdam <evan@rysdam.org>
2019-11-26 04:44:36 -0500

    Added colors to Mandelbrot game.

--- src/games/mandelbrot.s
+++ src/games/mandelbrot.s
@@ -36,22 +36,61 @@ put_color:
 	pop	{r4, r5, r6, pc}
 
 	
+// make_color: make a color out of a scalar. Start with black, then drift 
+// through blue, green, and finally red.
+//
+//	r0 . the scalar (range: 0 - 93)
+	
+make_color:
+	blue	.req r1
+	green	.req r2
+	red	.req r3
+
+	subs	blue, r0, #31		// blue = dist from r0 to 31
+	rsblos	blue, blue, #0
+	cmp	blue, #31		// blue = max(blue, 31)
+	movhi	blue, #31
+	rsb	blue, #31		// blue = 31 - blue
+
+	subs	green, r0, #62		// green = dist from r0 to 31
+	rsblos	green, green, #0
+	cmp	green, #31		// green = max(green, 31)
+	movhi	green, #31
+	rsb	green, #31		// green - 31 - green
+
+	subs	red, r0, #62		// red = dist that r0 is above 62
+	movlo	red, #0
+	cmp	red, #31
+	movhi	red, #31
+	
+	mov	r0, blue
+	add	r0, green, lsl #6
+	add	r0, red, lsl #11
+
+	bx	lr
+
+	.unreq	blue
+	.unreq	green
+	.unreq	red
+	
 // Mandelbrot: draw a Mandelbrot fractal on the screen.
 
 Mandelbrot:
 	push	{r4, r5, r6, lr}
 
-	mov	r4, #-200
-	mov	r5, #-100
-	ldr	r6, =0x0f0f		// greenish blue
+	mov	r4, #100
+	mov	r5, #94
 	
 m_loop$:
+	mov	r0, r5
+	bl	make_color
+	mov	r2, r0
+
 	mov	r0, r4
 	mov	r1, r5
-	mov	r2, r6
 	bl	put_color
 
-	subs	r4, #1
-	//bne	m_loop$
+	subs	r5, #1
+	bne	m_loop$
 
 	pop	{r4, r5, r6, pc}
