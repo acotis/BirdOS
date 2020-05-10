881e491ac687137077c13fa5915ccac91f3d9838
Evan Rysdam <evan@rysdam.org>
2020-05-10 07:03:42 -0400

    (Tiny transform commit #2)

--- transform/source/frameBuffer.s
+++ transform/source/frameBuffer.s
@@ -50,27 +50,11 @@ FrameBufferInfo:
 .section .text
 .globl InitialiseFrameBuffer
 InitialiseFrameBuffer:
-	width .req r0
-	height .req r1
-	bitDepth .req r2
-	cmp width,#4096
-	cmpls height,#4096
-	cmpls bitDepth,#32
 	result .req r0
-	movhi result,#0
-	movhi pc,lr
 
 	push {r4,lr}	
 	fbInfoAddr .req r4		
 	ldr fbInfoAddr,=FrameBufferInfo
-	//str width,[r4,#0]
-	//str height,[r4,#4]
-	//str width,[r4,#8]
-	//str height,[r4,#12]
-	//str bitDepth,[r4,#20]
-	.unreq width
-	.unreq height
-	.unreq bitDepth
 
 	mov r0,fbInfoAddr
 	add r0,#0x40000000
