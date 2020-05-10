622c9b31961b74dc3dd4406956036c51d96672a9
Evan Rysdam <evan@rysdam.org>
2020-05-10 08:33:55 -0400

    (Tiny transform commit #9)

--- transform/source/main.s
+++ transform/source/main.s
@@ -8,98 +8,43 @@
 *	main.s contains the main operating system, and IVT code.
 ******************************************************************************/
 
-/*
-* .globl is a directive to our assembler, that tells it to export this symbol
-* to the elf file. Convention dictates that the symbol _start is used for the 
-* entry point, so this all has the net effect of setting the entry point here.
-* Ultimately, this is useless as the elf itself is not used in the final 
-* result, and so the entry point really doesn't matter, but it aids clarity,
-* allows simulators to run the elf, and also stops us getting a linker warning
-* about having no entry point. 
-*/
-.section .init
+// Exports
+
 .globl _start
+
+// Code
+
+.section .init
+
 _start:
+    b main
 
-/*
-* Branch to the actual main code.
-*/
-b main
-
-/*
-* This command tells the assembler to put this code with the rest.
-*/
 .section .text
 
-/*
-* main is what we shall call our main operating system method. It never 
-* returns, and takes no parameters.
-* C++ Signature: void main(void)
-*/
 main:
+	mov     sp, #0x8000
+	bl      GPUInit
 
-/*
-* Set the stack point to 0x8000.
-*/
-	mov sp,#0x8000
+    pointer .req r0
+    color   .req r1
+    x       .req r2
+    y       .req r3
 
-/* NEW
-* Setup the screen.
-*/
-	mov r0,#1024
-	mov r1,#768
-	mov r2,#16
-	bl GPUInit
-
-/* NEW
-* Check for a failed frame buffer.
-*/
-	teq r0,#0
-	bne noError$
-		
-	mov r0,#16
-	mov r1,#1
-	bl SetGpioFunction
-
-	mov r0,#16
-	mov r1,#0
-	bl SetGpio
-
-	error$:
-		b error$
-
-	noError$:
-
-
-/* NEW
-* Set pixels forevermore. 
-*/
 render$:
-	fbAddr .req r3
     bl      GetFrameBufferPointer
-    mov     fbAddr, r0
 
-/* NEW
-* We will use r0 to keep track of the current colour.
-*/
-	colour .req r4
-	y .req r1
-	mov y,#768
-	drawRow$:
-		x .req r2
-		mov x,#1024
-		drawPixel$:
-			strh colour,[fbAddr]
-			add fbAddr,#2
-			sub x,#1
-			teq x,#0
-			bne drawPixel$
+	mov     y, #768
+drawRow$:
+    mov     x, #1024
+drawPixel$:
+    strh    color,[pointer]
+    add     pointer, #2
+    subs    x,#1
+    bne     drawPixel$
 
-		sub y,#1
-		add colour,#1
-		teq y,#0
-		bne drawRow$
+    add     color,#1
+    subs    y,#1
+    bne     drawRow$
 
-	b render$
+	b       render$
 
-	.unreq fbAddr
